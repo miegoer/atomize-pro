e7018df63bbeb816e58d36f3d2bb81a26025b098
Nesar Ahmad Nori <nesar.nori01@gmail.com>
2024-09-26 16:02:36 +0200

    index.ts content changed to ts, express and core type dependency added to dev dependency

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,27 @@
-const express = require('express');
-const cors = require("cors");
-const router = require('./router.js');
+import express, { Application } from 'express';
+import cors, { CorsOptions } from 'cors';
+import router from './routes/router';
 
-const corsOptions = {
+const corsOptions: CorsOptions = {
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow credentials (like cookies) to be sent
 };
 
-const app = express();
-
-const PORT = process.env.SERVER_PORT || 3000;
+const app: Application = express();
+const PORT: string | number = process.env.SERVER_PORT || 3000;
 
 app.use(cors(corsOptions));
 app.use(express.json());
-// app.use('/api', router);
 app.use('/api', router);
 
-app.listen(PORT, (err) => {
-    if (err) {
-      console.log(`😞 Something went wrong connecting to the server! ${err}`);
-    } else {
-      console.log(`🚀 The server is running and listening on port ${PORT}!`);
-    }
-  });
+app.listen(PORT, (err?: Error) => {
+  if (err) {
+    console.log(`😞 Something went wrong connecting to the server! ${err}`);
+  } else {
+    console.log(`🚀 The server is running and listening on port ${PORT}!`);
+  }
+});
+
+export default app;
