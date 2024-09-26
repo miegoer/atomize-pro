75ddaaf1c5d8ebb0b7f580cd61228b16da5946ff
Aidan <aidanl0919@gmail.com>
2024-09-26 18:37:53 +0100

    Refactor Column to tsx
    Refactor Column to generic tsx

--- client/src/components/dragdrop/Column.tsx
+++ client/src/components/dragdrop/Column.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 import './Column.css';
 import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import { Task } from "./Task";
 
-export const Column = ({tasks}) => {
+export const Column = ({tasks}: any) => {
     return (
         <div className="column">
         <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
-        {tasks.map(task => <Task id={task.id} name={task.name} key={task.id}/>)}
+        {tasks.map((task: any) => <Task id={task.id} name={task.name} key={task.id}/>)}
         </SortableContext>
         </div>
     )
