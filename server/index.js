867f5a38045c81bf8e95223c967b92d9e8268da3
José Coesel <j.e.coesel@gmail.com>
2024-09-27 09:27:25 +0200

    CreateNewGoal.js/CreateNewList.jsx/CreateNew.jsx refactored into Typescript

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const router = require('./router.js');
+const {connectDB} = require('./db.js');
 
 const corsOptions = {
   origin: 'http://localhost:5173',
@@ -11,17 +12,22 @@ const corsOptions = {
 
 const app = express();
 
+// Connect to the database (no need to use app.use)
+connectDB();
+
 const PORT = process.env.PORT || 3000;
 
+// Apply middleware
 app.use(cors(corsOptions));
 app.use(express.json());
-// app.use('/api', router);
+
+// Set up the router
 app.use('/api', router);
 
 app.listen(PORT, (err) => {
-    if (err) {
-      console.log(`😞 Something went wrong connecting to the server! ${err}`);
-    } else {
-      console.log(`🚀 The server is running and listening on port ${PORT}!`);
-    }
-  });
+  if (err) {
+    console.log(`😞 Something went wrong connecting to the server! ${err}`);
+  } else {
+    console.log(`🚀 The server is running and listening on port ${PORT}!`);
+  }
+});
